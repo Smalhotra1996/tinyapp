2c4549d08350e0efcbee2d9e3911ad61560cefd1
Sonam <sonam.malhotra2496@gmail.com>
2021-08-30 21:16:35 +0000

    Created ejs file in ejs-demo directory and  learned res.rendor

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 8080; //default port 8080
 
+
 const urlDatabase ={
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -19,6 +20,15 @@ app.get("/urls.json", (req,res) =>{
 app.get("/hello", (req,res) =>{
   res.send("<html><body><h1>Hello</h1><b>World</b></body>\n");
 
+});
+
+app.get("/set",(req,res) =>{
+  const a = 2;
+  res.send(`a = ${a}`);
+});
+
+app.get("/fetch",(req,res)=>{
+  res.send(`a = ${a}`);
 });
 
 app.listen(port , () => {
